47eb6c80bf6a4140653d62407683479a99fb89e0
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-02-07 05:28:07 -0300

    atualização

--- Js_inter_hernças_em_OO/index.js
+++ Js_inter_hernças_em_OO/index.js
@@ -1,19 +1,19 @@
-import {Cliente} from "./Cliente.js"               //importando
+import {Cliente} from "./Cliente.js"               
 import {ContaCorrente} from "./ContaCorrente.js"     
 import { ContaPoupanca } from "./ContaPoupanca.js";
-                                                   
+import { Conta } from "./Conta.js";                                      
 
 const cliente1 = new Cliente("Ricardo",11122233309);
 const cliente2 = new Cliente("Alice",88833377709);
 
+const cC1 = new ContaCorrente(cliente1, 1001);
+cC1.depositar(500);
+cC1.sacar(100);
 
-const conta1 = new ContaCorrente(cliente1,1001);
-conta1.depositar(500);
-conta1.sacar(100);
+const cC2 = new ContaCorrente(cliente2, 102);
+const cP1 = new ContaPoupanca(50, cliente1, 1001)
 
-const conta2 = new ContaCorrente(cliente2, 102);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+console.log(cP1);
+console.log(cC1);
 
-console.log(conta1);
-console.log(contaPoupanca);
 
