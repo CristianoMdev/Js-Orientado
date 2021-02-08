444ddcfa4981ca7918d61779d4a967674567796d
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-02-08 08:40:24 -0300

    alteração

--- Js_inter_hernças_em_OO/index.js
+++ Js_inter_hernças_em_OO/index.js
@@ -1,11 +1,9 @@
-import {Cliente} from "./Cliente.js"               
-import {ContaCorrente} from "./Conta/ContaCorrente.js"     
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./Conta/ContaCorrente.js"
 import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
 import { ContaSalario } from "./Conta/ContaSalario.js";
-                                 
 
-const cliente1 = new Cliente("Ricardo",11122233309);
-
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const cC1 = new ContaCorrente(cliente1, 1001);
 const cP1 = new ContaPoupanca(50, cliente1, 1001);
