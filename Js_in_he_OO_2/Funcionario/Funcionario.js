7c9aced1a9b03e4c550ab04070d98e5a4369b2d7
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-02-10 22:04:06 -0300

    atualização

--- Js_in_he_OO_2/Funcionario/Funcionario.js
+++ Js_in_he_OO_2/Funcionario/Funcionario.js
@@ -8,9 +8,9 @@ export class Funcionario {
         this.senha;
     }
 
-    get senha(){
-        return this._senha;
-    }
+  autenticar(senha){
+      return senha == this._senha;
+  }
 
     cadastrarSenha(senha){
         this._senha = senha;
--- Js_in_he_OO_2/index.js
+++ Js_in_he_OO_2/index.js
@@ -4,10 +4,12 @@ import { Diretor } from "./Funcionario/Diretor.js"
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js"
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
-const gerente = new Gerente("Carlos", 10000, 32132132100);
+const gerente = new Gerente("Carlos", 5000, 32132132100);
+const cliente = new Cliente("Camila", 55522233389, "321" );
 diretor.cadastrarSenha("123456789");
 gerente.cadastrarSenha("1234");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
-
-console.log(estaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const gernteEstaLogado = SistemaAutenticacao.login(gerente, "1234");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "321");
+console.log(diretorEstaLogado,  gernteEstaLogado, clienteEstaLogado);
